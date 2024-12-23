ac45951bbde99e0243f6e169c93241d223496014
shefat26 <shefatmoral@gmail.com>
2024-12-23 12:21:43 -0500

    my personal influencer gear project

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 
-const h2 = document.createElement("h2")
-h2.textContent = "This content added by JavaScript";
+// const h2 = document.createElement("h2")
+// h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+// document.querySelector("body").appendChild(h2);
+
+
+
+const preBook = document.getElementById("prebook");
+const seeMore = document.getElementById("seemore");
+const joinNow = document.getElementById("joinnow");
+
+
+preBook.addEventListener("mouseover",()=>{
+
+    preBook.style.backgroundColor = "blue";
+    
+   
+})
+
+
+preBook.addEventListener("mouseleave",()=>{
+
+    preBook.style.backgroundColor = "rgb(231, 11, 103)";
+    
+   
+})
+
+
+seeMore.addEventListener("click",()=>{
+
+    seeMore.style.backgroundColor = "green"
+ 
+})
+
+
+seeMore.addEventListener("mouseleave",()=>{
+
+    seeMore.style.backgroundColor = ""
+ 
+})
+
+
+
+
+
